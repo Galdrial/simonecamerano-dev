cc56634110d83661bd21fbea61857945fde0d378
Galdrial <camerano.simo@gmail.com>
2025-10-23 05:03:04 +0200

    Refactor script to improve readability by consolidating sed command block

--- convert_absolute_to_relative.sh
+++ convert_absolute_to_relative.sh
@@ -3,16 +3,16 @@
 # Converte i percorsi assoluti in relativi nei file HTML
 
     
+find docs -name "*.html" | while read file; do
+  sed -i \
+    -e 's|href="/|href="|g' \
+    -e 's|src="/assets/img|src="assets/img|g' \
+    -e 's|src="/assets|src="assets|g' \
+    -e 's|src="/img|src="img|g' \
+    -e 's|srcset="/assets/img|srcset="assets/img|g' \
+    -e 's|srcset="/assets|srcset="assets|g' \
+    -e 's|url(/assets/img|url(assets/img|g' \
+    -e 's|url(/assets|url(assets|g' \
+    -e 's|url(/img|url(img|g' \
+    "$file"
 done
-    find docs -name "*.html" | while read file; do
-      sed -i \
-        -e 's|href="/|href="|g' \
-        -e 's|src="/assets/img|src="assets/img|g' \
-        -e 's|src="/assets|src="assets|g' \
-        -e 's|src="/img|src="img|g' \
-        -e 's|srcset="/assets/img|srcset="assets/img|g' \
-        -e 's|srcset="/assets|srcset="assets|g' \
-        -e 's|url(/assets/img|url(assets/img|g' \
-        -e 's|url(/assets|url(assets|g' \
-        -e 's|url(/img|url(img|g' \
-        "$file"
