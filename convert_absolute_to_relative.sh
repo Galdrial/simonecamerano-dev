1f50bd9c50d39dfce1fc7ef5d788adc7cad48262
Galdrial <camerano.simo@gmail.com>
2025-10-23 04:57:15 +0200

    Refactor script to improve readability and maintainability by adjusting indentation and organizing sed commands

--- convert_absolute_to_relative.sh
+++ convert_absolute_to_relative.sh
@@ -2,15 +2,15 @@
 # Script: convert_absolute_to_relative.sh
 # Converte i percorsi assoluti in relativi nei file HTML
 
-find docs -name "*.html" | while read file; do
+    
+    find docs -name "*.html" | while read file; do
   sed -i \
     -e 's|href="/|href="|g' \
+    -e 's|src="/assets/img|src="assets/img|g' \
     -e 's|src="/assets|src="assets|g' \
     -e 's|src="/img|src="img|g' \
+    -e 's|url(/assets/img|url(assets/img|g' \
     -e 's|url(/assets|url(assets|g' \
     -e 's|url(/img|url(img|g' \
     "$file"
 done
-    -e 's|src="/assets/img|src="assets/img|g' \
-    -e 's|url(/assets/img|url(assets/img|g' \
-    -e 's|url(/assets|url(assets|g' \
